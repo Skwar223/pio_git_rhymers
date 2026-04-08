c60c8546fbbf62ecca173001f4b0f18ff398fdc9
unknown <255654@edu.p.lodz.pl>
2026-04-08 16:30:10 +0200

    17. dokumentacja w FIFORhymers

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,16 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa działajaca w oparciu o metode FIFO.
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
     private DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+
+    /**
+     * Metoda która zwraca elemnt zgodnie z zasada FIFO
+     */
     @Override
     public int countOut() {
         while (!callCheck())
